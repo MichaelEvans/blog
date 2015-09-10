81f7aca75bd2f81151a6fba1487285990f300585
Michael Evans <michaelcevans10@gmail.com>
2015-09-09 20:10:18 -0400

    Fix speakerdeck plugin

--- plugins/speakerdeck.rb
+++ plugins/speakerdeck.rb
@@ -7,7 +7,7 @@ module Jekyll
         end
 
         def render(context)
-            %(<script async="true" class="speakerdeck-embed" data-id="#{@id.strip}" src="//speakerdeck.com/assets/embed.js"> </script>)
+            %(<script async="true" class="speakerdeck-embed" data-id="#{@id.strip}" data-ratio="1.77777777777778" src="//speakerdeck.com/assets/embed.js"> </script>)
         end
 
     end
